c0cf6548196db7e8be385bbe5b5de3edfcadbc67
Zakary <zackarybamba@gmail.com>
2022-06-06 16:44:43 -0700

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *print a phrase
+ *Return : 0 if exited normaly, non-zero otherwise
+ */
+int main(void)
+{
+  puts("Programming is like building a multilingual puzzle"\n);
+  return (0);
+}  
+  
